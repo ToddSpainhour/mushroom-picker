01f56b344fb14fd0c1f66c2d71413284b949950f
Todd Spainhour <toddspainhour@gmail.com>
2020-05-24 16:14:03 -0500

    added rules toggle

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import mushroomData from '../helpers/data/mushroomData';
 import Forest from '../components/Forest/Forest';
 import Basket from '../components/Basket/Basket';
+import RulesToggle from '../components/RulesToggle/RulesToggle';
 
 class App extends React.Component {
   // outside the render() seen below is where you put anything that modifies state
@@ -15,29 +16,32 @@ state = {
 componentDidMount() {
   const mushrooms = mushroomData.getMushrooms();
   const basket = mushroomData.getBasket();
-  // console.error('your mushrooms are...', mushrooms);
   this.setState({ mushrooms, basket });
 }
 
-// this is essentially saying this.pickAMushroom. Outside the render() don't used const
 
 pickAMushroom = () => {
   mushroomData.pickAMushroom();
   const mushrooms = mushroomData.getMushrooms();
   const basket = mushroomData.getBasket();
-  this.setState({ mushrooms }); // this resets the state aka reprints
-  this.setState({ basket }); // this resets the state aka reprints
+  this.setState({ mushrooms });
+  this.setState({ basket });
 }
 
+showTheRules = () => {
+  mushroomData.showTheRules();
+}
+
+
 render() {
-  // inside the render() is for anything that modifies the UI
   const { mushrooms, basket } = this.state;
 
   return (
     <div className="App">
       <h2 className='mushroom-title'>Mushroom Picker</h2>
-      <Forest mushrooms={mushrooms} pickAMushroom={this.pickAMushroom}/>
-      <Basket basket={basket} mushrooms={mushrooms} pickAMushroom={this.pickAMushroom}/>
+      <RulesToggle />
+      <Forest mushrooms={mushrooms} pickAMushroom={this.pickAMushroom} />
+      <Basket basket={basket} mushrooms={mushrooms} pickAMushroom={this.pickAMushroom} />
     </div>
   );
 }
---
+++ src/components/RulesToggle/RulesToggle.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './RulesToggle.scss';
+
+class RulesToggle extends React.Component {
+  state = {
+    visible: false,
+  }
+
+toggle = () => {
+  this.setState({
+    visible: !this.state.visible,
+  });
+}
+
+render() {
+  return (
+      <div>
+        <button className="btn btn-outline-dark btn-sm" onClick={this.toggle}>Show/Hide Rules</button>
+        {this.state.visible
+         && <div><h6>A random mushroom will be selected when you click the button below. There are 15 normal mushrooms, 3 poisonous, and 1 deadly.</h6>
+         <h6>There is also a single magic mushroom, and if you pick it, you win the game.</h6>
+</div>
+              }
+      </div>
+
+  );
+}
+}
+
+export default RulesToggle;
