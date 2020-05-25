1d2eb1d47c7a7458c3439669af320d183a6bcbbd
Todd Spainhour <toddspainhour@gmail.com>
2020-05-25 00:19:33 -0500

    added scss color variables

--- src/components/Mushroom/Mushroom.js
+++ src/components/Mushroom/Mushroom.js
@@ -17,8 +17,8 @@ class MushroomCard extends React.Component {
       <div className="MushroomCard col-3">
   <img src={mushroom.imgUrl} className="card-img-top" alt="Mushroom Card" />
   <div className="card-body">
-    <h4 className="card-title text-body">{mushroom.name}</h4>
-    <p className="card-text text-body">{mushroom.description}</p>
+    <h4 className="mushroom-name card-title">{mushroom.name}</h4>
+    <p className="mushroom-discription card-text">{mushroom.description}</p>
   </div>
 </div>
     );
