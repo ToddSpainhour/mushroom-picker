f91125c7ab25469d40418b6741cf2ff84c593815
Todd Spainhour <toddspainhour@gmail.com>
2020-05-25 19:36:31 -0500

    more styling added

--- src/components/RulesToggle/RulesToggle.js
+++ src/components/RulesToggle/RulesToggle.js
@@ -18,7 +18,7 @@ render() {
       <div>
         <button className="btn btn-outline btn-sm rules-toggle-btn" onClick={this.toggle}>Show/Hide Rules</button>
         {this.state.visible
-         && <div><h6>A random mushroom will be selected when you click the button below. There are 15 normal mushrooms, 3 poisonous, and 1 deadly.</h6>
+         && <div className="rules-div"><h6>A random mushroom will be selected when you click the button below. There are 15 normal mushrooms, 3 poisonous, and 1 deadly.</h6>
          <h6>There is also a single magic mushroom, and if you pick it, you win the game.</h6>
 </div>
               }
